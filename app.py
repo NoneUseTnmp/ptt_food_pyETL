7e44a4402f059acb1a44a284c2b28b9937df9933
johnson18527 <84448990+johnson18527@users.noreply.github.com>
2021-07-18 16:10:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent as ua
 url='https://www.ptt.cc/bbs/Food/index.html'
-useragent ='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56'
-headers = {'User-Agent':useragent}
+ua1=ua().random
+headers = {'User-Agent':"".format(ua1)}
 import time
 import random
 import pandas as pd
+
 
 columns=['標題', '網址', '內文','推','噓', '分數','作者','時間']
 data=[]
@@ -13,7 +15,7 @@ data=[]
 ss=requests.session()
 
 
-for l in range(0,3):
+for l in range(0,1):
     res =None 
     while True:
         try:
@@ -29,6 +31,8 @@ for l in range(0,3):
         try:
             title =i.select('a')[0].text
             arturl= "https://www.ptt.cc/"+i.select('a')[0]['href']
+            ua2=ua().random
+            headers = {'User-Agent':"".format(ua2)}
             artres=ss.get (arturl,headers= headers)
             artsoup = BeautifulSoup(artres.text,'html.parser')
             conten = artsoup.select('div[id="main-content"]')[0].text.split('※ 發信站')[0]
@@ -48,7 +52,7 @@ for l in range(0,3):
             
             rdata=[title,arturl,conten,up,down,sour,ats,tt]
             data.append(rdata)
-            time.sleep(random.randint(1,10))
+            time.sleep(random.randint(1,5))
             
         except IndexError as e:
             print(i)
